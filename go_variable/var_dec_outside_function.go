91b9037b23abc1127751e100ad32e67ab4ae46c6
Sazeda Akter <96733495+sazeda1@users.noreply.github.com>
2022-03-23 21:32:09 +0600

    Create var_dec_outside_function.go

---
+++ go_variable/var_dec_outside_function.go
@@ -0,0 +1,14 @@
+//variable declaration outside of a function
+package main
+import ("fmt")
+ 
+var a int
+var b int = 2
+var c = 3
+ 
+func main() {
+  a = 1
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+}
